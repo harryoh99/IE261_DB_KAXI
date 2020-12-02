ca680c009d75d9206110d6412490b59fddc612ae
Jio_SamsungPC <harryoh99@naver.com>
2020-12-02 23:56:13 +0900

    Fin

---
+++ ex1/taxi_right_main.php
@@ -0,0 +1,11 @@
+<html>
+   <head meta charset="utf-8">
+   <title> Main </title>
+   </head>
+   <body>
+      <br><br>
+      <p style = 'text-align:center;'> <B> KAXI 사이트 사용설명서!! </B></p>
+      <br>
+      <p style = 'text-align:center;'> <img src = 'int3.jpg'> </p>
+   </body>
+</html>
