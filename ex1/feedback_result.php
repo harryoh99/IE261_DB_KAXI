1fdb8abc843cb4583c24c4ab57385a69f45e6bfa
Jio_SamsungPC <harryoh99@naver.com>
2020-12-02 19:18:45 +0900

    tmp

--- ex1/feedback_result.php
+++ ex1/feedback_result.php
@@ -14,14 +14,17 @@
 	
 	
 	
-	if($text!=' ') {$sql= "insert into feedbacktbl(userid,rating,feedback) values";
-					$sql= $sql."('$id','$rate','$text');";
-					$result=mysqli_query($conn,$sql);
+	if($text!=' ') {
+		$sql= "insert into feedbacktbl(userid,rating,feedback) values";
+		$sql= $sql."('$id','$rate','$text');";
+		$result=mysqli_query($conn,$sql);
 					
-					if($result){
-								echo '<script type="text/javascript">alert("Successfully Registered"); location.replace("Feedback_list.php");</script>';}
-					else{
-								echo '<script type="text/javascript">alert("Register fail"); location.replace("feedback.php");</script>';}
+		if($result){
+			echo '<script type="text/javascript">alert("Successfully Saved"); location.replace("Feedback_list.php");</script>';
+		}
+		else{
+			echo '<script type="text/javascript">alert("Save fail"); location.replace("feedback.php");</script>';
+		}
 	}
 	else{
 		echo '<script type="text/javascript">alert("fill the content!!"); location.replace("feedback.php");</script>';}
--- ex1/main.php
+++ ex1/main.php
@@ -26,7 +26,7 @@
 					<title> Kaxi </title>
 					</head>
 					<FRAMESET ROWS = "120, *">
-						<FRAME SRC = "top.html" NAME = "top" noresize>
+						<FRAME SRC = "top.php" NAME = "top" noresize>
 						<FRAMESET COLS = "200, *">
 							<FRAME SRC = "menu.html" NAME = "left" noresize>
 							<FRAME SRC = "right_main.php" NAME = "right" noresize>
