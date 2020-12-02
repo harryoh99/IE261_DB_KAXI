62af1bc1845a9fcf590a47f9d4dfabd5a691af44
choimoon11 <74757439+choimoon11@users.noreply.github.com>
2020-12-02 16:52:39 +0900

    Update feedback_result.php

--- ex1/feedback_result.php
+++ ex1/feedback_result.php
@@ -1,7 +1,40 @@
 <?php
+	session_start();
     $servername = 'localhost';
     $username = 'root';
     $pw = '1234';
     $db = 'kaxi';
+	
+	
+	$conn = mysqli_connect($servername, $username, $pw, $db) or die ('MySQL connect failed');
+
+	$id= $_SESSION['ID'];
+	$text=$_POST['text'];
+	$rate=$_POST['rating'];
+	
+	
+	
+	if($text!=' ') {$sql= "insert into feedbacktbl(userid,rating,feedback) values";
+					$sql= $sql."('$id','$rate','$text');";
+					$result=mysqli_query($conn,$sql);
+					
+					if($result){
+								echo '<script type="text/javascript">alert("Successfully Registered"); location.replace("Feedback_list.php");</script>';}
+					else{
+								echo '<script type="text/javascript">alert("Register fail"); location.replace("feedback.php");</script>';}
+	}
+	else{
+		echo '<script type="text/javascript">alert("fill the content!!"); location.replace("feedback.php");</script>';}
+	
+	
+
+
+
+
+
+
+
+
+
 
 ?>
